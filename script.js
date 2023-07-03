ca2eeded125e8aa16085da8be29044e9361ef0fa
lingvvarm <lingvvarm@gmail.com>
2023-07-03 18:54:48 +0300

    Add github link and fight visuals

--- script.js
+++ script.js
@@ -56,38 +56,48 @@ function game(play) {
     let cpu_score = document.querySelector('.cpu-score');
     let msg_text = document.querySelector('.msg-text');
     let msg_subtext = document.querySelector('.msg-subtext');
+    let player_image = document.getElementById('player-img');
+    let cpu_image = document.getElementById('cpu-img');
+    let fightBlock = document.querySelector('.fight');
+    let game_message = document.querySelector('.game-message');
+    player_image.src = `img/${play}.png`;
+    cpu_image.src = `img/${computerSelection}.png`;
+    game_message.classList.remove('hidden');
+    fightBlock.classList.remove('hidden');
+    
     
     if (winner == "player") {
         let new_score = Number(player_score.textContent) + 1;
         player_score.textContent = String(new_score);
-        msg_text.textContent = `Your ${capitalize(play)} beats Computer's ${capitalize(computerSelection)}`;
+        msg_text.textContent = `Your ${capitalize(play)} beats computer's ${capitalize(computerSelection)}`;
         msg_subtext.textContent = "You win!";
     }
     else if (winner == "cpu") {
         let new_score = Number(cpu_score.textContent) + 1;
         cpu_score.textContent = String(new_score);
-        msg_text.textContent = `Computer's ${capitalize(computerSelection)} beats Your ${capitalize(play)}`;
+        msg_text.textContent = `Your ${capitalize(play)} is beaten by computer's ${capitalize(computerSelection)}`;
         msg_subtext.textContent = "You lose!";
     }
     else {
         msg_text.textContent = `It's a tie!`;
-        msg_subtext.textContent = "";
+        msg_subtext.textContent = "Try once more!";
+    }
+
+    function clear() {
+        player_score.textContent = "0";
+        cpu_score.textContent = "0";
+        game_message.classList.add('hidden');
+        fightBlock.classList.add('hidden');
     }
 
     setTimeout(() => {
         if (player_score.textContent == 5) {
             alert("You won. Congtatulations!");
-            player_score.textContent = "0";
-            cpu_score.textContent = "0";
-            msg_subtext.textContent = "";
-            msg_text.textContent = "";
+            clear();
         }
         else if (cpu_score.textContent == 5) {
             alert("You lost. Try again!");
-            player_score.textContent = "0";
-            cpu_score.textContent = "0";
-            msg_subtext.textContent = "";
-            msg_text.textContent = "";
+            clear();
         }
         
     }, 0);
